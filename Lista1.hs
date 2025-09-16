037120be6981c1be24ecc4202b6a56b89481aac4
Kailani <90580550+kaikai-maf@users.noreply.github.com>
2025-09-16 14:08:58 -0300

    Update Lista1.hs

--- Lista1.hs
+++ Lista1.hs
@@ -11,28 +11,30 @@ tipoTriangulo x y z | x == y && y == z = "Equilatero"
 -- EX.3: now, we merge both of the previous funcions to verify wether it is or not a triangle, also classifying what kind it is
 triangulo x y z | ehTriangulo x y z = tipoTriangulo x y z
                 | otherwise  = "Nao eh"
--- EX. 4: sum of all numbers up to n
+-- EX. 4: 
 --verify if given number is even
 ehPar x
     |mod x 2 == 0 =  True
     |otherwise = False
+
+-- sum of all numbers up to n
 somaPares 0 = 0
 somaPares n 
     |n < 2 = 0
     |ehPar n  = n + somaPares (n-1)
     |otherwise = somaPares (n-1)
 
--- exercicio 5
+-- EX. 5: sum of 2 powered up to n, times m
 
 somaPot2m m 0 = m
 somaPot2m m n = somaPot2m m (n-1) + (2^n) * m
 
--- exericio 6
+-- EX. 6: verify wether given number is prime or not
 primo x
     | x > 0 && mod x 2 /= 0 || mod x 2 == 1 = True
     | otherwise =  False
 
--- exercicio 7
+-- EX. 7: 
 seriePI n |n == 1 = 4/1
           |mod n 2 == 0 = seriePI(n-1)
           |mod (div n 2) 2 == 0 = seriePI(n-2) + (4/fromIntegral n)
